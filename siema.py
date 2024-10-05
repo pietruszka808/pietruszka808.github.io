2838988afe01f8a84013abc8b98686ddf22a544f
Piotr Hiszpanski <piotr.hiszpanski23@gmail.com>
2024-10-05 21:39:00 +0200

    add siema.py

---
+++ siema.py
@@ -0,0 +1,15 @@
+import tkinter
+
+
+playerX = "X"
+playerO = "O"
+curr_player = playerX
+board = [[0, 0, 0],[0, 0, 0],[0, 0, 0]] 
+
+color_blue = "#4584b6"
+color_yellow = "#ffde57"
+color_gray = "#343434"
+color_light_gray = "#646464"
+
+window = tkinter.Tk()
+window.title("Tic Tac Toe")
