ba78058f94feb36882e998cc80b89a6fb2cd9133
Piotr Hiszpanski <piotr.hiszpanski23@gmail.com>
2024-10-06 20:12:16 +0200

    dodałem window

--- siema.py
+++ siema.py
@@ -3,6 +3,9 @@ import tkinter
 def set_tile(row, column):
     pass
 
+def new_game():
+    pass
+
 playerX = "X"
 playerO = "O"
 curr_player = playerX
@@ -33,6 +36,23 @@ for row in range(3):
                                             command=lambda row=row, column=column: set_tile(row, column))
         board[row][column].grid(row=row+1, column=column)
 
+button = tkinter.Button(frame, text="restart", font=("Consolas", 20), background=color_gray,
+                        foreground="white", command=new_game)
+button.grid(row=4, column=0, columnspan=3, sticky="we")
+
 frame.pack()
+
+
+window.update()
+window_width = window.winfo_width()
+window_haight = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_haight = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_haight/2) - (window_haight/2))
+
+
+window.geometry(f"{window_width}x{window_haight}+{window_x}+{window_y}")
 
 window.mainloop()
