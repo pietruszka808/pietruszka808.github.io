84daa9f4f2201e77be0df090ed83ee470441b582
Piotr Hiszpanski <piotr.hiszpanski23@gmail.com>
2024-11-06 19:56:43 +0100

    dodalem okno

---
+++ eloelo.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+def button_press(num):
+    pass
+
+def equals():
+    pass
+
+def clear():
+    pass
+
+window = Tk()
+window.title("Calcurator program")
+window.geometry("500x500")
+
+equation_text = ""
+
+
+
+window.mainloo()
