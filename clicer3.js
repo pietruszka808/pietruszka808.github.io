361d873109a57e1a558b6c3e5f6ccd4a3e8f42fb
Pietruszka808 <piotr.hiszpanski23@gmail.com>
2025-02-23 13:35:51 +0100

    nie dziala

--- clicer3.js
+++ clicer3.js
@@ -1,5 +1,13 @@
 let gem = document.querySelector('.gem-cost')
 
+let clickerCost = document.querySelector('.clicker-cost')
+
 function incrementGem() {
     gem.innerHTML = parseFloat(gem.innerHTML) + 1
+}
+
+function buyClicker() {
+    if (gem.innerHTML >= clickerCost.innerHTML) {
+        gem.innerHTML -= clickerCost.innerHTML
+    }
 }
