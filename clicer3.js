7a3c59116e95ce2a1cbd0952c2455fe3223a8fc9
Pietruszka808 <piotr.hiszpanski23@gmail.com>
2025-02-23 18:32:33 +0100

    dgfvfvfvf

--- clicer3.js
+++ clicer3.js
@@ -7,22 +7,44 @@ let clickerLevel = document.querySelector(".clicker-level")
 let clickerIncrease = document.querySelector(".clicker-increase")
 let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
 
+let pickaxeCost = document.querySelector('.pickaxe-cost');
+let parsedPickaxeCost = parseFloat(pickaxeCost.innerHTML);
+let pickaxeLevel = document.querySelector(".pickaxe-level")
+let pickaxeIncrease = document.querySelector(".pickaxe-increase")
+let parsedpickaxeIncrease = parseFloat(pickaxeIncrease.innerHTML)
+
 let gpc = 1;
 
 function incrementGem() {
-    parsedGem += gpc;
-    gem.innerHTML = parsedGem;
+    gem.innerHTML = Math.round(parsedGem += gpc);
 }
 
 function buyClicker() {
-    if (parsedGem >= parsedClickerCost) {
-      parsedGem -= parsedClickerCost;   
-      gem.innerHTML = parsedGem;
+    if (parsedGem >= parsedClickerCost) {   
+      gem.innerHTML = Math.round(parsedGem -= parsedClickerCost);
 
       clickerLevel.innerHTML ++
 
-      parsedClickerIncrease = parsedClickerIncrease * 1.03
+      parsedClickerIncrease = parseFloat((parsedClickerIncrease * 1.03).toFixed(2))
       clickerIncrease.innerHTML = parsedClickerIncrease
       gpc += parsedClickerIncrease
+
+      parsedClickerCost *= 1.18;
+      clickerCost.innerHTML = Math.round(parsedClickerCost)
     }
+}
+
+function buyPickaxe() {
+  if (parsedGem >= parsedPickaxeCost) {   
+    gem.innerHTML = Math.round(parsedGem -= parsedPickaxeCost);
+
+    pickaxeLevel.innerHTML ++
+
+    parsedPickaxeIncrease = parseFloat((parsedPickaxeIncrease * 1.03).toFixed(2))
+    pickaxeIncrease.innerHTML = parsedPickaxeIncrease
+    gpc += parsedPickaxeIncrease
+
+    parsedPickaxeCost *= 1.18;
+    pickaxeCost.innerHTML = Math.round(parsedPickaxeCost)
+  }
 }
