4d94253e0da33c50516c3b48da3667c472ca0ea7
Pietruszka808 <piotr.hiszpanski23@gmail.com>
2025-02-23 17:29:34 +0100

    nie dziala

--- clicer3.js
+++ clicer3.js
@@ -1,17 +1,28 @@
-let gem = document.querySelector('.gem-cost')
+let gem = document.querySelector('.gem-cost');
 let parsedGem = parseFloat(gem.innerHTML)
 
-let clickerCost = document.querySelector('.clicker-cost')
-let parsedClickerCost = parseFloat(clickerCost.innerHTML)
+let clickerCost = document.querySelector('.clicker-cost');
+let parsedClickerCost = parseFloat(clickerCost.innerHTML);
+let clickerLevel = document.querySelector(".clicker-level")
+let clickerIncrease = document.querySelector(".clicker-increase")
+let parsedClickerIncrease = parseFloat(clickerIncrease.innerHTML)
+
+let gpc = 1;
 
 function incrementGem() {
-    parsedGem += 1
-    gem.innerHTML = parsedGem
+    parsedGem += gpc;
+    gem.innerHTML = parsedGem;
 }
 
 function buyClicker() {
     if (parsedGem >= parsedClickerCost) {
-      parsedGem -= parsedClickerCost   
-      gem.innerHTML = parsedGem
+      parsedGem -= parsedClickerCost;   
+      gem.innerHTML = parsedGem;
+
+      clickerLevel.innerHTML ++
+
+      parsedClickerIncrease = parsedClickerIncrease * 1.03
+      clickerIncrease.innerHTML = parsedClickerIncrease
+      gpc += parsedClickerIncrease
     }
 }
